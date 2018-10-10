84d30a9df93f5220f8e3fa35d756a41c622f1324
Paoonline <paoonline@windowslive.com>
2018-10-10 08:17:31 +0700

    Initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,33 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () => {
+    return(
+        <div>
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">
+                            React Time app
+                        </li>
+                        <li>
+                            <IndexLink to="/" activeClassname="active-link">Timer</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/" activeClassname="active-link">Countdown</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <ul className="menu">
+                        <li className="menu-text">
+                            Create by <a href="http://www.google.co.th" target="_blank">Pao</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+module.exports = Navigation
